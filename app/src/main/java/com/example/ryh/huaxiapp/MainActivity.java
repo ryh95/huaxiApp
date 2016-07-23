f5bae9f03cfa4e5578ac2f920e89613994717766
ryh <1971990184@qq.com>
2016-07-23 16:38:22 +0800

    input date,download file in the external storage directory /down

--- app/src/main/java/com/example/ryh/huaxiapp/MainActivity.java
+++ app/src/main/java/com/example/ryh/huaxiapp/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.ryh.huaxiapp;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.BinaryHttpResponseHandler;
+import com.loopj.android.http.FileAsyncHttpResponseHandler;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button download,read;
+    private EditText date;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +29,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         download = (Button) findViewById(R.id.download);
+        date = (EditText) findViewById(R.id.date);
 
         download.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String text = "this is download";
-                Toast.makeText(getApplicationContext(),text,Toast.LENGTH_SHORT).show();
+                asyncGet(date.getText().toString());
             }
         });
+    }
+
+    private void asyncGet(String date){
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://101.201.114.42/"+date+"result.xls";
+        File file = Environment.getExternalStorageDirectory();
+        FileAsyncHttpResponseHandler f = new FileAsyncHttpResponseHandler(new File(file,"down/"+date+"result.xls")) {
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, File file) {
+                Toast.makeText(MainActivity.this,"Failed!",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, File file) {
+                Toast.makeText(MainActivity.this,"Succeed! The document is in the /down ",Toast.LENGTH_SHORT).show();
+            }
+        };
+        client.get(url,f);
     }
 }
