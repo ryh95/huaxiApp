ec198ab17a0b77faa2a61eb6ef4f4e235911c114
ryh <1971990184@qq.com>
2016-08-03 23:18:14 +0800

    add SwipeRefreshLayout

--- app/src/main/java/com/example/ryh/huaxiapp/MainActivity.java
+++ app/src/main/java/com/example/ryh/huaxiapp/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.ryh.huaxiapp;
 
 import android.content.Intent;
 import android.os.Environment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -17,6 +19,11 @@ import com.loopj.android.http.FileAsyncHttpResponseHandler;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 import cz.msebera.android.httpclient.Header;
 import jxl.Sheet;
@@ -25,34 +32,40 @@ import jxl.read.biff.BiffException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button download,read;
-    private EditText date;
-
+    private SwipeRefreshLayout swipeContainer;
+    private TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        download = (Button) findViewById(R.id.download);
-        date = (EditText) findViewById(R.id.date);
+        final DateFormat dateFormat = new SimpleDateFormat("MM-dd");
+        final Calendar cal = Calendar.getInstance();
+        cal.add(Calendar.DATE, -1);
 
-        download.setOnClickListener(new View.OnClickListener() {
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        txt = (TextView)findViewById(R.id.excel);
+
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+
+            String date =  dateFormat.format(cal.getTime());
+
             @Override
-            public void onClick(View v) {
-                asyncGet(date.getText().toString());
+            public void onRefresh() {
+                asyncGet(date);
             }
         });
 
-        read = (Button) findViewById(R.id.read);
-        read.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                readExcel(date.getText().toString());
-            }
-        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
+
     }
 
-    private void asyncGet(String date){
+    private void asyncGet(final String date){
         AsyncHttpClient client = new AsyncHttpClient();
         String url = "http://101.201.114.42/"+date+"result.xls";
         File sdCard = Environment.getExternalStorageDirectory();
@@ -65,14 +78,34 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Header[] headers, File file) {
                 Toast.makeText(MainActivity.this,"Succeed! The document is in the /down ",Toast.LENGTH_SHORT).show();
+                readExcel(date);
             }
         };
         client.get(url,f);
     }
 
-    private void readExcel(String date) {
-        Intent i = new Intent(MainActivity.this,ReadExcelActivity.class);
-        i.putExtra("date",date);
-        startActivity(i);
+    public void readExcel(String date) {
+        try {
+            File sdCard = Environment.getExternalStorageDirectory();
+            Workbook book = Workbook.getWorkbook(new File(sdCard,"down/"+date+"result.xls"));
+            int num = book.getNumberOfSheets();
+            txt.setText("the date is " + date+  "\n");
+            // 获得第一个工作表对象
+            Sheet sheet = book.getSheet(0);
+            int Rows = sheet.getRows();
+            int Cols = sheet.getColumns();
+            for (int i = 0; i < Rows; ++i) {
+                for (int j = 0; j < Cols; ++j) {
+                    // getCell(Col,Row)获得单元格的值
+//                    txt.append("contents:" + sheet.getCell(i,j).getContents() + "\n");
+                    txt.append(sheet.getCell(j,i).getContents()+"   ");
+                }
+                txt.append("\n");
+            }
+            book.close();
+            swipeContainer.setRefreshing(false);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
